6aea022d6c5f599905e55eda5e05d15e19f6ecfb
Abdimaan <hamaan3858@gmail.com>
2024-08-26 12:33:04 +0300

    update one

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -11,19 +11,22 @@ const buttonholder = 80.0;
 const activecolor = Color(0xFF1D1E33);
 const inactivecolor = Color(0xFF111328);
 const buttoncolor = Color(0xFFEB1555);
+
 enum Gender {
   male,
   female,
 }
+
 class InputPage extends StatefulWidget {
   @override
   InputPageState createState() => InputPageState();
 }
+
 class InputPageState extends State<InputPage> {
   Gender? selectedgender;
-  double height=180;
- double  weight= 60;
-  int age=20;
+  double height = 180;
+  double weight = 60;
+  int age = 20;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +34,7 @@ class InputPageState extends State<InputPage> {
         backgroundColor: Color(0xFF0A0E21),
         title: Center(
           child: Text(
-            "BMI Calculator",
+            "Abdirahman Ahmed Mohamed",
             style: TextStyle(
               fontSize: 50,
               fontWeight: FontWeight.bold,
@@ -48,12 +51,14 @@ class InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: Reusable(
-                    onpress:(){
+                    onpress: () {
                       setState(() {
                         selectedgender = Gender.male;
                       });
-                    } ,
-                    color: selectedgender == Gender.male ? activecolor : inactivecolor,
+                    },
+                    color: selectedgender == Gender.male
+                        ? activecolor
+                        : inactivecolor,
                     cardchild: IconContent(
                       icons: FontAwesomeIcons.mars,
                       label: 'MALE',
@@ -62,12 +67,14 @@ class InputPageState extends State<InputPage> {
                 ),
                 Expanded(
                   child: Reusable(
-                    onpress: (){
+                    onpress: () {
                       setState(() {
-                        selectedgender =Gender.female;
+                        selectedgender = Gender.female;
                       });
                     },
-                    color: selectedgender == Gender.female ? activecolor : inactivecolor,
+                    color: selectedgender == Gender.female
+                        ? activecolor
+                        : inactivecolor,
                     cardchild: IconContent(
                       icons: FontAwesomeIcons.venus,
                       label: 'FEMALE',
@@ -78,172 +85,168 @@ class InputPageState extends State<InputPage> {
             ),
           ),
           Expanded(
-            child: Reusable(color: activecolor,
-            cardchild: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("HEIGHT",
-                style: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  color: Colors.white,
-                  fontSize: 25.0,
-                ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.baseline,
-                  textBaseline: TextBaseline.alphabetic,
-                  children: [
+            child: Reusable(
+              color: activecolor,
+              cardchild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
                   Text(
-                    height.toString(),
-                  style: TextStyle(
-                    fontWeight: FontWeight.w900,
-                    fontSize: 50,
-                    color: Colors.white,
+                    "HEIGHT",
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                      fontSize: 25.0,
+                    ),
                   ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(
+                        height.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 50,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        'CM',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
                   ),
-                  Text('CM',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize:15,
-                    fontWeight: FontWeight.w600, 
-                  ),
-                  
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      inactiveTrackColor: Color(0xFF8D8E98),
+                      activeTrackColor: Colors.white,
+                      overlayColor: Color(0x99EB1555),
+                      thumbColor: Colors.pink,
+                      thumbShape:
+                          RoundSliderThumbShape(enabledThumbRadius: 20.0),
+                      overlayShape: RoundSliderOverlayShape(overlayRadius: 30),
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      min: 120.0,
+                      max: 230.0,
+                      onChanged: (double newvalue) {
+                        setState(() {
+                          height = newvalue;
+                        });
+                      },
+                    ),
                   ),
                 ],
-                ),
-                SliderTheme(
-                  data: SliderTheme.of(context).copyWith(
-                    inactiveTrackColor:Color(0xFF8D8E98), 
-                    activeTrackColor: Colors.white,
-                    overlayColor: Color(0x99EB1555),
-                    thumbColor: Colors.pink,
-                    thumbShape: RoundSliderThumbShape(enabledThumbRadius: 20.0),
-                    overlayShape: RoundSliderOverlayShape(overlayRadius: 30),
-                  ),
-                  child: Slider(
-                    value: height.toDouble(),
-                     min: 120.0,
-                     max: 230.0,
-                     onChanged: (double newvalue){
-                      setState(() {
-                        height = newvalue;
-                      });
-                     },
-                    ),
-                ),
-              ],
-            ),
-            
+              ),
             ),
           ),
           Expanded(
             child: Row(
               children: [
-              Expanded(
-                  child: Reusable(color: activecolor,
-                  cardchild: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'WEIGHT',
-                        style: TextStyle(
-                     fontWeight: FontWeight.normal,
-                      color: Colors.white,
-                    fontSize: 25.0,
-                ),
-                      ),
-                      Text(
-                        weight.toString(),
-                        style:TextStyle(
-                    color: Colors.white,
-                    fontSize:15,
-                    fontWeight: FontWeight.w600, 
-                  ),
-                  
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          IconButton(
-                            onPressed: (){
-                              setState(() {
-                                weight--;
-                              });
-                            },
-                             icon:Icon(FontAwesomeIcons.minus),
-                             ),
-                           SizedBox(
-                            width: 10.0,
-                           ),
-                           IconButton(
-                            onPressed:(){
-                            setState(() {
-                              weight++;
-                            });
-                            } ,
-                            icon: Icon(FontAwesomeIcons.plus),
+                Expanded(
+                  child: Reusable(
+                    color: activecolor,
+                    cardchild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'WEIGHT',
+                          style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            color: Colors.white,
+                            fontSize: 25.0,
+                          ),
+                        ),
+                        Text(
+                          weight.toString(),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  weight--;
+                                });
+                              },
+                              icon: Icon(FontAwesomeIcons.minus),
                             ),
-
-                        ],
-                      ),
-                      
-                    ],
-                  ),
-                  
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  weight++;
+                                });
+                              },
+                              icon: Icon(FontAwesomeIcons.plus),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
                 Expanded(
-                  child: Reusable(color: activecolor,
-                  cardchild: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'AGE',
-                        style: TextStyle(
-                     fontWeight: FontWeight.normal,
-                      color: Colors.white,
-                    fontSize: 25.0,
-                ),
-                      ),
-                      Text(
-                        age.toString(),
-                        style:TextStyle(
-                    color: Colors.white,
-                    fontSize:15,
-                    fontWeight: FontWeight.w600, 
-                  ),
-                  
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          IconButton(
-                            onPressed: (){
-                              setState(() {
-                                age--;
-                              });
-                            },
-                             icon:Icon(FontAwesomeIcons.minus),
-                             ),
-                           SizedBox(
-                            width: 10.0,
-                           ),
-                           IconButton(
-                            onPressed:(){
-                            setState(() {
-                              age++;
-                            });
-                            } ,
-                            icon: Icon(FontAwesomeIcons.plus),
+                  child: Reusable(
+                    color: activecolor,
+                    cardchild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'AGE',
+                          style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            color: Colors.white,
+                            fontSize: 25.0,
+                          ),
+                        ),
+                        Text(
+                          age.toString(),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  age--;
+                                });
+                              },
+                              icon: Icon(FontAwesomeIcons.minus),
                             ),
-
-                        ],
-                      ),
-                      
-                    ],
-                  ),
-                  
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  age++;
+                                });
+                              },
+                              icon: Icon(FontAwesomeIcons.plus),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
@@ -251,20 +254,18 @@ class InputPageState extends State<InputPage> {
           ),
           Button(
             buttontitle: 'CALCULATE',
-            onTap:(){
-              Bmilogic cal =Bmilogic(height: height, weight: weight);
+            onTap: () {
+              Bmilogic cal = Bmilogic(height: height, weight: weight);
 
               Navigator.push(
-                context, 
-              MaterialPageRoute(
-                builder: (context) => Resultpage(
-                  bmiresult:cal.calculateBmi(),
-                  resulttext: cal.getResult(),
-                  interpre: cal.getInterpretation(),
-                )
-              )
-              );
-            } ,
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => Resultpage(
+                            bmiresult: cal.calculateBmi(),
+                            resulttext: cal.getResult(),
+                            interpre: cal.getInterpretation(),
+                          )));
+            },
           ),
         ],
       ),
